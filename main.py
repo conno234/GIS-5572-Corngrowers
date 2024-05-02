c9449e151979542c1743906d26ddf00d0c9a0106
conno234 <135058788+conno234@users.noreply.github.com>
2024-05-01 23:45:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+rom flask import Flask, jsonify
+import psycopg2 
+import json
+import os
 
+app = Flask(__name__)
+
+db_params = {
+    'database': 'final_project',
+    'user': 'postgres',  
+    'password': 'gisishard2024', 
+    'host': '34.132.23.62',
+    'port': '5432'
+}
+
+
+@app.route('/idw_et_point')
+def get_idw_temp_point():
+    try:
+        table_name = "idw_et_point"
+        geom_column = "shape"
+        geojson = fetch_geom_as_geojson(table_name, geom_column, db_params)
+
+        return jsonify(geojson)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8080)
